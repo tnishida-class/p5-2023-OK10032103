796e160c940fa0c0e55b986c6d75acbd0fa863b1
OK10032103 <146839843+OK10032103@users.noreply.github.com>
2023-10-09 22:57:59 +0900

    Change section 1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -5,10 +5,10 @@ function setup() {
 function draw() {
   background(0);
   strokeWeight(4);
-  fill(178, 100, 162);
+  fill(100, 30, 50);
   triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
+  fill(90, 160, 23);
+  triangle(0,99,22,108,106,0);
   fill(255);
   textSize(32);
   textFont("serif");
